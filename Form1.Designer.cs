f3ab5f061eab0616affea0f99739a92baac2fb9e
GatoWare <138316044+OxyZin@users.noreply.github.com>
2026-03-07 20:08:34 -0300

    fixed the icon

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -109,6 +109,7 @@
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "LegacyConsoleLauncher";
